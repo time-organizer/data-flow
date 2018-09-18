34abb8df56bf9c472753c9334235acdc414e5f8b
Michał Grabowski <Miriloth@gmail.com>
2018-09-18 21:51:04 +0200

    Add morgan

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+
 const app = express();
 app.use(cors());
+app.use(morgan('dev'));
 
 require('./db/index');
 
