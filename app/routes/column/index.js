7cb2e019d34c2e0b113110f56698a9ee9d5e6f46
Michał Grabowski <Miriloth@gmail.com>
2019-01-23 15:45:52 +0100

    Add column update route

--- app/routes/column/index.js
+++ app/routes/column/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const create = require('./create');
+const update = require('./update');
 // const get = require('./get');
 // const remove = require('./remove');
 const app = express();
 
 app.use(create);
+app.use(update);
 // app.use(get);
 // app.use(remove);
 
---
+++ app/routes/column/update.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const logger = require('../../../logger');
+
+const verifyToken = require('../../middlewares/verifyToken');
+const Column = require('../../models/Column');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+router.put('/columns/:columnId', verifyToken, (req, res) => {
+  const { columnId } = req.params;
+  const { updatedObject, confirmOnly } = req.body;
+  const updatedData = {
+    ...updatedObject,
+    lastUpdated: new Date(),
+  };
+
+  Column.findByIdAndUpdate(columnId, updatedData, { upsert: true, new: true })
+    .then((updatedColumn) => {
+      return confirmOnly
+        ? res.status(200).send({ message: 'Successfully updated column' })
+        : res.status(200).send(updatedColumn);
+    })
+    .catch((error) => {
+      logger.error(error);
+      return res.status(500).send({ messasge: 'Server error' });
+    });
+});
+
+module.exports = router;
