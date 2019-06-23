343a7ce9e7f2cf8cfda4344d4379cab097173052
Michał Grabowski <Miriloth@gmail.com>
2019-06-23 12:10:20 +0200

    reconnect to db

--- db/index.js
+++ db/index.js
@@ -1,4 +1,16 @@
 const mongoose = require('mongoose');
 
 const DB_ADDRESS = process.env.DB_ADDRESS;
-mongoose.connect(DB_ADDRESS, { useNewUrlParser: true });
+
+const connect = () => {
+  mongoose.connect(DB_ADDRESS, { useNewUrlParser: true })
+    .then(() => {
+      console.log('Connected to db');
+    })
+    .catch(() => {
+      setTimeout(connect, 5000);
+      console.log('Trying to reconnect to db in 5s');
+    });
+};
+
+connect();
